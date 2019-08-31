bbe74fb186aff68b78bf26d3f927db5056efed1e
Pradeep Singh <pdeepsingh91@gmail.com>
2019-08-31 18:06:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="pre-process",
     version="0.0.1",
     author="Pradeep Singh",
     author_email="pdeepsingh91@gmail.com",
